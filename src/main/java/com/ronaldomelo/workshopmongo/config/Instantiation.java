fc12f1b0e73ebf156f6f3735ff5af1ef8211b0bf
ronaldo-melo <rpm.melkor@gmail.com>
2020-05-14 14:09:27 -0300

    usando padrao DTO para retornar usuários

--- src/main/java/com/ronaldomelo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/ronaldomelo/workshopmongo/config/Instantiation.java
@@ -18,9 +18,11 @@ public class Instantiation implements CommandLineRunner {
 	@Override
 	public void run(String... arg0) throws Exception {
 		userReposiroty.deleteAll();
+		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 	}
 
--- src/main/java/com/ronaldomelo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ronaldomelo/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.ronaldomelo.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ronaldomelo.workshopmongo.domain.User;
+import com.ronaldomelo.workshopmongo.dto.UserDTO;
 import com.ronaldomelo.workshopmongo.services.UserService;
 
 @RestController
@@ -19,8 +21,11 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		
+		List<UserDTO> listFto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listFto);
 	}
 }
