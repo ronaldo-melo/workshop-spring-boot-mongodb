9d675c40576c850ab073b65d5b1c0c5a5aedc673
ronaldo-melo <rpm.melkor@gmail.com>
2020-05-23 15:25:56 -0300

    inserção de usuário com POST

--- src/main/java/com/ronaldomelo/workshopmongo/services/UserService.java
+++ src/main/java/com/ronaldomelo/workshopmongo/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
 		repo.deleteById(id); 
 	}
 	
+	public User update(User obj) {
+		User newObj = repo.findById(obj.getId()).get();
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getEmail());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
