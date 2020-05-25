6d94a13a42a7518d3c3bc4cab9123288eab39154
ronaldo-melo <rpm.melkor@gmail.com>
2020-05-24 23:54:25 -0300

    obtendo post por id

--- src/main/java/com/ronaldomelo/workshopmongo/resources/PostResource.java
+++ src/main/java/com/ronaldomelo/workshopmongo/resources/PostResource.java
@@ -16,7 +16,8 @@ public class PostResource {
 
 	@Autowired
 	private PostService service;
-
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
