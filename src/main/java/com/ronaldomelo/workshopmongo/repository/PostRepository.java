5fe77a89a3bfd1cbf62284aa88d1b63166f4a0b3
ronaldo-melo <rpm.melkor@gmail.com>
2020-05-25 21:40:09 -0300

    Consulta simples com query methods

--- src/main/java/com/ronaldomelo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/ronaldomelo/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.ronaldomelo.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.ronaldomelo.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
